eca0120d896ee2e8781b4dd92082574884b8becf
SMOOVPOS_HOAN\trqua <hoan.tran@smoovapp.com>
2017-01-04 09:16:27 +0700

    timing relay

--- ht-garden-timing/ht-garden-timing.ino
+++ ht-garden-timing/ht-garden-timing.ino
@@ -1,14 +1,34 @@
-#define relayPin 2
+// These variables store the flash pattern
+// and the current state of the LED
+ 
+int ledPin =  2;      // the number of the LED pin
+int ledState = LOW;             // ledState used to set the LED
+unsigned long previousMillis = 0;        // will store last time LED was updated
+long milliseconds = 1000;
+long OnTime = 5*60*milliseconds;           // milliseconds of on-time
+long OffTime = 15*60*milliseconds;          // milliseconds of off-time
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(relayPin, OUTPUT);
+void setup() 
+{
+  // set the digital pin as output:
+  pinMode(ledPin, OUTPUT);      
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(relayPin, HIGH);
-  delay(3000);
-  digitalWrite(relayPin, LOW);
-  delay(3000);
+ 
+void loop()
+{
+  // check to see if it's time to change the state of the LED
+  unsigned long currentMillis = millis();
+ 
+  if((ledState == HIGH) && (currentMillis - previousMillis >= OnTime))
+  {
+    ledState = LOW;  // Turn it off
+    previousMillis = currentMillis;  // Remember the time
+    digitalWrite(ledPin, ledState);  // Update the actual LED
+  }
+  else if ((ledState == LOW) && (currentMillis - previousMillis >= OffTime))
+  {
+    ledState = HIGH;  // turn it on
+    previousMillis = currentMillis;   // Remember the time
+    digitalWrite(ledPin, ledState);    // Update the actual LED
+  }
 }
